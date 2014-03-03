2e041d5a1a1c6d1dffed68f91ecbf7972c288273
Zhang Yi <zhangyi.cn@gmail.com>
2014-03-03 09:54:27 +0800

    Initial Commit

---
+++ WechatExpert/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WechatExpert
+//
+//  Created by User #⑨ on 14-3-3.
+//  Copyright (c) 2014年 Yang Chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WechatExpertTests/WechatExpertTests.m
@@ -0,0 +1,34 @@
+//
+//  WechatExpertTests.m
+//  WechatExpertTests
+//
+//  Created by User #⑨ on 14-3-3.
+//  Copyright (c) 2014年 Yang Chao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WechatExpertTests : XCTestCase
+
+@end
+
+@implementation WechatExpertTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
